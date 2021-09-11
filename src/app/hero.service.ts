7a9c1c8477bd516f3d6f630c39d5d3846ec66973
tniu <tniu@infomedia.com.au>
2021-09-11 16:17:04 +1000

    init

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,7 +13,8 @@ export class HeroService {
 
   // private heroesUrl = 'api/heroes';  // URL to web api
   private heroesUrl = 'http://localhost:5000/list'
-  private addHeroesUrl = 'http://localhost:5000/add'
+  private getHeroUrl = 'http://localhost:5000/list'
+  private addHeroUrl = 'http://localhost:5000/add'
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -73,7 +74,7 @@ export class HeroService {
 
   /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.addHeroesUrl, hero, this.httpOptions).pipe(
+    return this.http.post<Hero>(this.addHeroUrl, hero, this.httpOptions).pipe(
       tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
